45a68fce58d404d6a2f9c960e952313cef85e9e5
wwerto543 <kovalevskiv2020@gmail.com>
2026-05-09 16:58:32 +0300

    Delete proxy_flagship_bot directory

--- proxy_flagship_bot/states/proxy_states.py
+++
@@ -1,6 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class ProxyFilter(StatesGroup):
-    confirm_sub = State()        # Ожидание подписки
-    choosing_filter = State()    # Выбор типа фильтрации
-    picking_country = State()    # Выбор конкретной страны
